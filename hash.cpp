0f0739fef23e5f864f9ef0cd0eeac2fd4147e039
Roman <romaferrero@hotmail.com>
2024-09-09 08:58:39 -0300

    Cambios en el TAD hash

--- hash.cpp
+++ hash.cpp
@@ -1,227 +1,151 @@
 #include <iostream>
-
 using namespace std;
 
-template <class T>
-class NodoHash
-{
-public:
-	T dato;
-	int cantidadElem;
-	int puntos;
-	NodoHash<T> *sig;
-	NodoHash(T dato, int cantidadElem, NodoHash<T> *sig) : dato(dato), cantidadElem(cantidadElem), puntos(0), sig(sig) {}
-	NodoHash(T dato) : dato(dato), cantidadElem(0), puntos(0), sig(NULL) {}
-	~NodoHash(){
-		if(sig){
-			delete sig;
-		}
-	}
+template<class K, class V>
+class NodoHash {
+    public:
+        K clave;
+        V valor;
+        bool susuki;
+        NodoHash(K clave, V valor) : clave(clave), valor(valor), susuki(false){}
 };
 
-template <class T>
-class Hash
-{
-private:
-	NodoHash<T> **vec;
-	int cota;
-	int cantidad;
-	int (*funcionHash)(T, int); 
-	void borrarh(NodoHash<T> *&l, T i)
-	{
-		if (l != NULL)
-		{
-			if (l->dato == i)
-			{
-				NodoHash<T> *aBorrar = l;
-				l = l->sig;
-				delete aBorrar;
-			}
-			else
-			{
-				borrarh(l->sig, i);
-			}
-		}
-	}
 
-public:
-	Hash(int cota, int (*func)(T, int))
-	{
-		this->cota = cota;
-		this->funcionHash = func;
-		this->cantidad = 0;
-		this->vec = new NodoHash<T> *[cota];
-		for (int i = 0; i < cota; vec[i++] = NULL)
-			;
-	}
+template<class K, class V>
+class Hash {
+    private:
+        NodoHash<K,V>** hash;
+        int cantidad;
+        int largoArray;
+        int maxCant;
+        int (*fHash)(K);
 
-	~Hash()
-	{
-		for (int i = 0; i < cota; i++){
-			if(vec[i]){
-				delete vec[i];
-			}	
-		}
-		cota = 0;
-		delete[] vec;
-	}
+        bool esPrimo(int num) {
+            if(num <= 1) {
+                return false;
+            } else {
+                for (int i = 2; i < num/2; i++)
+                {
+                    if(num%i==0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
 
-	void agregar(Hash <T>* &d, T dato)
-	{
-		if (!pertenece(d, dato))
-		{
-			NodoHash<T> *nuevo = new NodoHash<T>(dato);
-			// nuevo->dato = dato;
-			nuevo->sig = d->vec[d->funcionHash(dato, d->cota)];
-			d->vec[d->funcionHash(dato, d->cota)] = nuevo;
-			d->cantidad++;
-		}
-	}
+        int primoSup(int num){
+            while(!esPrimo(++num));
+            return num;
+            // 302782: Pedro Chialanza
+        }
 
-	void borrar(Hash<T>* &d, T dato)
-	{
-		if (pertenece(d, dato))
-		{
-			borrarh(d->vec[d->funcionHash(dato, d->cota)], dato);
-			d->cantidad--;
-		}
-	}
+    public:
+        Hash(int maxCant, int (*fHash)(K)) {
+            this->cantidad = 0;
+            this->maxCant = maxCant;
+            this->largoArray = primoSup(maxCant*2);
+            this->hash = new NodoHash<K,V>*[this->largoArray]();
+            this->fHash = fHash;
+        }   
 
-	bool pertenece(Hash<T> *d, T dato)
-	{
-		NodoHash<T> *h = d->vec[d->funcionHash(dato, d->cota)];
-		while (h != NULL && h->dato != dato)
-		{
-			h = h->sig;
-		}
-		return h != NULL;
-	}
+        ~Hash() {
+            for (int i = 0; i < largoArray; i++)
+            {
+                if(hash[i]){
+                    delete hash[i];
+                }
+            }
+            
+            delete [] this->hash;
+        }   
 
-	NodoHash<T>* elemento(Hash<T> * &d, T dato)
-	{
-		if (pertenece(d, dato)){
-			NodoHash<T> *h = d->vec[d->funcionHash(dato, d->cota)];
-			while (h != NULL && h->dato != dato)
-			{
-				h = h->sig;
-			}
-			if(h != NULL){
-			return h;
-			}
-		}
-		return NULL;
-	}
+        // Pre: !EsLleno()
+        void Insertar(K clave, V valor) {
+            int posHash = abs(fHash(clave)) % largoArray;
+            while(hash[posHash] && !hash[posHash]->susuki){
+                posHash = (posHash + 1) % largoArray;
+            }
+            if(hash[posHash]){
+                delete hash[posHash];
+            }
+            hash[posHash] = new NodoHash<K, V>(clave, valor);
+            cantidad++;
+        }
 
-//Pre: pertenece(Hash<T> *d, T dato)
-	void agregarOcurrencia(Hash<T> *&d, T dato){
-		NodoHash<T> *h = d->vec[d->funcionHash(dato, d->cota)];
-		h->cantidadElem++;
-	}
-//Pre: pertenece(Hash<T> *d, T dato)
-	void eliminarOcurrencia(Hash<T> *&d, T dato){
-		NodoHash<T> *h = d->vec[d->funcionHash(dato, d->cota)];
-		h->cantidadElem--;
-	}
+        // Pre: Existe(clave)
+        void Eliminar(K clave) {
+            int posHash = abs(fHash(clave)) % largoArray;
+            while(hash[posHash]->clave != clave){
+                posHash = (posHash + 1) % largoArray;
+            }
+            hash[posHash]->susuki = true;
+        }
 
-	int cantOcurrencias(Hash<T> *&d, T dato){
-	NodoHash<T> *h = d->vec[d->funcionHash(dato, d->cota)];
-	return h->cantidadElem;
-	}
+        // Pre: Existe(clave)
+        V Recuperar(K clave) {
+            int posHash = abs(fHash(clave)) % largoArray;
+            while(hash[posHash]->clave!=clave){
+                posHash = (posHash + 1) % largoArray;
+            }
+            return hash[posHash]->valor;
+        }
 
-	bool esVacio(Hash<T> *d)
-	{
-		return (d->cantidad == 0);
-	}
+        bool Existe(K clave) {
+            int posHash = abs(fHash(clave)) % largoArray;
+            while(hash[posHash] && hash[posHash]->clave!=clave){
+                posHash = (posHash + 1) % largoArray;
+            }
+            return hash[posHash] && !hash[posHash]->susuki;
+        }
 
-	unsigned int cantidadElementos(Hash<T> *d)
-	{
-		return d->cantidad;
-	}
+        void Inspeccionar(){
+            for (int i = 0; i < largoArray; i++)
+            {
+                cout << i <<":";
+                if(!hash[i]){
+                    cout << "<VACIO>";
+                } else if(hash[i]->susuki) {
+                    cout << "<BANIO URGENTE>";
+                } else {
+                    cout << "K: " << hash[i]->clave << " - V: " << hash[i]->valor;
+                }
+                cout << endl;
+            }
+            
+        }
 
-	Hash* clon(Hash <T>* d, int (*func)(T, int))
-	{
-		Hash<T> *ret = new Hash<T>(d->cota, func);
-		for (int i = 0; i < ret->cota; i++)
-		{
-			NodoHash<T> *alias = d->vec[i];
-			while (alias != NULL)
-			{
-				agregar(ret, alias->dato);
-				alias = alias->sig;
-			}
-		}
-		return ret;
-	}
-
-	int mostrar(Hash <T>*d)
-	{
-		if (!esVacio(d))
-		{
-			for (int i = 0; i < d->cota; i++)
-			{
-				if (d->vec[i] != NULL)
-				{
-					cout << d->vec[i]->dato << endl;
-				}
-			}
-		}
-		return 0;
-	}
-	/*void destruir(Hash& d) {
-		for (int i = 0; i <d->cota; i++)
-			destruirh(d->vec[i]);
-		delete[] d->vec;
-		delete d;
-	}*/
 };
 
-int fun(int num, int cota)
-{
-	return num % cota;
+// Ref: https://cseweb.ucsd.edu/~kube/cls/100/Lectures/lec16/lec16-15.html
+int fHash(string key) {
+  int h = 0;
+  for (int i = 0; i < key.length(); i++)
+    h = 31 * h + int(key[i]);
+  return h;
 }
 
-int main1()
-{
-	Hash<int> *h = new Hash<int>(8, fun);
-
-	h->agregar(h, 1);
-	h->agregar(h, 2);
-	h->agregar(h, 3);
-	h->agregar(h, 4);
-	h->agregar(h, 5);
-	h->agregar(h, 6);
-	h->agregar(h, 7);
-	h->mostrar(h);
-	NodoHash<int>* l=h->elemento(h, 3);
-	cout << "cantidad de 3 :" ;
-	cout << l->cantidadElem << endl;
-	l->cantidadElem+=22;
-	cout << "cantidad de 3 modifcado con + 22 :" ;
-	cout << l->cantidadElem << endl;
-	cout << "l es :" << endl;
-	cout <<  l->dato << endl;
-	if (h->pertenece(h, 2))
-	{
-		cout << "ok" << endl;
-	}
-	h->borrar(h, 2);
-	if (h->pertenece(h, 2))
-	{
-		cout << "Si esta el 2" << endl;
-	}
-	else
-	{
-		cout << "No esta el 2" << endl;
-	}
-	h->mostrar(h);
-	cout << "esp" << endl;
-	Hash<int> *h2 = h->clon(h, fun);
-	h->mostrar(h2);
-	cout << "agrego 2" << endl;
-	h->agregar(h2,2);
-	h->mostrar(h2);
-	cout << "hola" << endl;
-	h->mostrar(h);
-
-	return 0;
-};
+int main() {
+    int largo, ret = 0;
+    string palabra;
+    cin >> largo;
+    Hash<string, int>* hash = new Hash<string, int>(largo, fHash);
+    for (int i = 0; i < largo; i++)
+    {
+        cin >> palabra;
+        bool existePalabra = hash->Existe(palabra);
+        if(!existePalabra){
+            hash->Insertar(palabra, 1);
+        } else {
+            int ocurrencias = hash->Recuperar(palabra);
+            if(ocurrencias == 1) {
+                ret++;
+            } else if (ocurrencias == 2) {
+                ret--;
+            }
+            hash->Eliminar(palabra);
+            hash->Insertar(palabra, ocurrencias+1);
+        }
+    }
+    cout << ret;
+}
