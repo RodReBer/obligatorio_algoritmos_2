b4339fa7c185d4ad1408e357e8c4999da6d31460
RodReBer <rodrigorey2005@gmail.com>
2024-09-25 09:39:24 -0300

    empezamos ejercicio 5

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -7,6 +7,31 @@ using namespace std;
 
 int main()
 {
-    // TODO
+    cin >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> idMision;
+        cin >> nombreMision;
+    }
+
+    cin >> c;
+    cin >> o;
+
+    for (int i = 0; i < c; i++)
+    {
+        cin >> idCiudad;
+        cin >> nombreCiudad;
+
+    }
+
+    cin >> e;
+
+    for (int i = 0; i < e; i++)
+    {
+        cin >> idCiudadOrigen;
+        cin >> idCiudadDestino;
+        cin >> tiempoDesplazamiento;
+    }
     return 0;
 }
